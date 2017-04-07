43a009aaea96ba823259bd7b27d73ecabf2cee92
Shawn Meyer <sgmeyer@gmail.com>
2017-04-07 17:21:25 -0500

    Initial commit

---
+++ app/auth.config.ts
@@ -0,0 +1,12 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string,
+    callbackURL: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'YOUR_CLIENT_ID',
+    domain: 'YOUR_DOMAIN',
+    // You may need to change this!
+    callbackURL: 'http://localhost:3000/'
+};
